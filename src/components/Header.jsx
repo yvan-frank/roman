97e8cebeeb18f2a966905afe29ee5285097b9549
yvan-frank <yvanzangue@gmail.com>
2023-11-02 23:37:54 +0100

    header config

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,6 +2,8 @@ import React, {useState, useEffect} from 'react'
 import logo from '../assets/logo rapid.jpg'
 import { NavLink, useNavigate } from 'react-router-dom'
 import { root, searchLink } from '../routes'
+import playLogo from "../assets/playstore.png";
+import appleLogo from "../assets/apple.png";
 
 export default function Header() {
     const [query, setQuery] = useState('')
@@ -29,12 +31,38 @@ export default function Header() {
   return (
     <div className={`w-full top-0 py-2 bg-white ${scrollY > 400 && 'sticky transition duration-500 shadow-xl'}`}>
         <div className='flex items-center justify-between px-8'>
-            <NavLink to={root} className='w-full'>
+            <NavLink to={root} className='w-96'>
                 <img className='w-20' src={logo} alt="" />
             </NavLink>
             {/* menu */}
             <div className='w-full'>
-                <NavLink>Catalogue</NavLink>
+
+                <div className='flex flex-row items-center justify-between'>
+                    <NavLink to={root}>Accueil</NavLink>
+                    <NavLink>Catalogue</NavLink>
+                    <NavLink>Sélections</NavLink>
+
+                   <div className='flex mr-5'>
+                       <div className='flex p-1 w-[7.5rem] bg-black cursor-pointer items-center rounded text-white/80'>
+                           <div className='mr-1'>
+                               <img className='w-6' src={playLogo} alt="" />
+                           </div>
+                           <div className='flex flex-col'>
+                               <span className='text-[0.6rem] -tracking-wider uppercase'>disponible sur</span>
+                               <span className=' font-bold text-sm'>Google Play</span>
+                           </div>
+                       </div>
+                       <div className='flex p-1 w-[7.5rem] bg-black cursor-pointer items-center rounded text-white/80 ml-2'>
+                           <div className='mr-1'>
+                               <img className='w-6' src={appleLogo} alt="" />
+                           </div>
+                           <div className='flex flex-col'>
+                               <span className='text-[0.6rem] -tracking-wider'>Télécharger dans</span>
+                               <span className=' font-bold text-sm'>l'App Store</span>
+                           </div>
+                       </div>
+                   </div>
+                </div>
             </div>
             <form className='w-full' onSubmit={submit}>
                 <input 
