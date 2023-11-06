6f55a48cd60a39f5372e2296d7b135f3b4fce124
yvan-frank <yvanzangue@gmail.com>
2023-11-07 00:24:57 +0100

    SEO started

---
+++ src/components/Head.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import {Helmet} from "react-helmet";
+
+const Head = ({pageTitle,title, url, description, image, keywords, author}) => {
+	return (
+		<Helmet>
+			{/* Placez ici vos méta données */}
+			{/*<title> {title} </title>*/}
+			<meta name="description" content={description} />
+			<meta name="keywords" content={keywords} />
+			<meta name="author" content={author} />
+			{/* Protocole OpenGraph */}
+			<meta property="og:title" content={title} />
+			<meta property="og:locale" content="fr_CAMEROUN" />
+			<meta property="og:type" content="website" />
+			<meta property="og:image" content={image} />
+			<meta property="og:url" content={url} />
+			<meta property="og:description" content={description} />
+		</Helmet>
+	);
+};
+
+export default Head;
--- src/pages/public/AboutPage.jsx
+++ src/pages/public/AboutPage.jsx
@@ -1,5 +1,7 @@
 import React, {useEffect} from 'react';
 import GeneralHeader from "../../components/index.jsx";
+import Head from "../../components/Head.jsx";
+import {useLocation} from "react-router-dom";
 
 const AboutPage = () => {
 	useEffect(() => {
@@ -7,6 +9,11 @@ const AboutPage = () => {
 	}, []);
 	return (
 		<div>
+			<Head
+				title='Qui sommes-nous | Rabipek Novel'
+				description='Rabipek Novel est une plateforme de lecture de roman'
+				url={window.location.href}
+			/>
 			<GeneralHeader>
 				<div className='h-screen'>
 					<div className='flex flex-col max-w-6xl mx-auto mt-20'>
